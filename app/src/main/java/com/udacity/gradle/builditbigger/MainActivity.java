a88b39394ebebb7be442bc369941cbcba24a2d87
Lars Braitinger <lars-braitinger@web.de>
2018-01-19 20:43:46 +0100

    add step 1

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.javajokes.JokeTeller;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, new JokeTeller().tellAJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ javaJokes/src/main/java/com/example/javajokes/JokeTeller.java
@@ -0,0 +1,7 @@
+package com.example.javajokes;
+
+public class JokeTeller {
+    public String tellAJoke(){
+            return "Why do Java programmers have to wear glasses? - Because they don't C#.";
+        }
+}
